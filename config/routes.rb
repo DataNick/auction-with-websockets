13202f27eaf18b037322fa62ca44154850f1ccdf
DataNick <DataNick@users.noreply.github.com>
2016-07-15 07:08:28 -0400

    Nested auction routes inside products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+
   devise_for :users
-  resources :products
+  resources :products do
+    resources :auctions, only: [ :create ]
+  end
   root 'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
