7c9316da2df0917a4520b899af09976cb65bc015
DataNick <DataNick@users.noreply.github.com>
2016-07-15 07:10:42 -0400

    Generated bid model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715055814) do
+ActiveRecord::Schema.define(version: 20160715110710) do
 
   create_table "auctions", force: :cascade do |t|
     t.float    "value"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20160715055814) do
   end
 
   add_index "auctions", ["product_id"], name: "index_auctions_on_product_id"
+
+  create_table "bids", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "auction_id"
+    t.float    "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "bids", ["auction_id"], name: "index_bids_on_auction_id"
+  add_index "bids", ["user_id"], name: "index_bids_on_user_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
