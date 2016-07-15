fb24cefa128f4acc062cb9f3adc34c2945ca3987
DataNick <DataNick@users.noreply.github.com>
2016-07-15 01:57:49 -0400

    When user creates product, user_id is associated with product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+    @product = Product.new(product_params.merge! user_id: current_user.id)
 
     respond_to do |format|
       if @product.save
