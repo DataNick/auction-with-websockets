17da02425df4627fc3e8986d8fd4c98d90fae017
DataNick <DataNick@users.noreply.github.com>
2016-07-15 18:15:34 -0400

    Included has_many products association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_many :products
+  has_many :products
 end
