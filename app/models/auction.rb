50faf9b5d9989c399a49c576c242944c3696347e
DataNick <DataNick@users.noreply.github.com>
2016-07-15 18:14:19 -0400

    Created method ended?

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,3 +1,16 @@
 class Auction < ActiveRecord::Base
   belongs_to :product
+  has_many :bids
+
+  def top_bid
+    bids.order(value: :desc).first
+  end
+
+  def current_bid
+    top_bid.nil? ? value : top_bid.value
+  end
+
+  def ended?
+    Time.now > ends_at
+  end
 end
