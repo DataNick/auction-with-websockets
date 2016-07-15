2ddaec4733c07ff17aac66e9329fc6ce5877e70c
DataNick <DataNick@users.noreply.github.com>
2016-07-15 01:40:27 -0400

    Modified mailer configuration; set up sass as preferred extension when generating stylesheets

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Bunchostuff
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.sass.preferred_syntax = :sass
   end
 end
