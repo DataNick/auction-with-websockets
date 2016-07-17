a735ed6fdf90e6c99ec27887c7df2715ce986f2a
DataNick <DataNick@users.noreply.github.com>
2016-07-17 16:20:31 -0400

    Created notify_auction_ended method

--- lib/auction_socket.rb
+++ lib/auction_socket.rb
@@ -3,13 +3,14 @@ require File.expand_path "../place_bid", __FILE__
 class AuctionSocket
   def initialize(app)
     @app = app
+    @clients = []
   end
 
   def call(env)
     @env = env
     if socket_request?
       socket = spawn_socket
-
+      @clients << socket
       socket.rack_response
     else
       @app.call(env)
@@ -57,8 +58,27 @@ class AuctionSocket
 
     if service.execute
       socket.send("bidok")
+      notify_outbids(socket, tokens.last)
     else
-      socket.send("underbid #{service.auction.current_bid}")
+      if service.status == :won
+        notify_auction_ended(socket)
+      else
+        socket.send("underbid #{service.auction.current_bid}")
+      end
+    end
+  end
+
+  def notify_outbids(socket, value)
+    @clients.reject { |client| client == socket }.each do |client|
+      client.send("outbid #{value}")
+    end
+  end
+
+  def notify_auction_ended(socket)
+    socket.send("won")
+
+    @clients.reject {|client| client == socket}. each do |client|
+      client.send("lost")
     end
   end
 end
