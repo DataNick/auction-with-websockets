de1439d513c212c1a0e2732155cd7f9adda83ab9
DataNick <DataNick@users.noreply.github.com>
2016-07-16 20:26:10 -0400

    Modified js file

--- app/assets/javascripts/auction_websockets.js
+++ app/assets/javascripts/auction_websockets.js
@@ -1,4 +1,4 @@
-var AuctionSocket = function(user_id, auction_id, form){
+var AuctionSocket = function(user_id, auction_id, form) {
   this.user_id = user_id;
   this.auction_id = auction_id;
   this.form = $(form);
@@ -8,14 +8,20 @@ var AuctionSocket = function(user_id, auction_id, form){
   this.initBinds();
 };
 
-
-AuctionSocket.prototype.initBinds = function(){
+AuctionSocket.prototype.initBinds = function() {
   var _this = this;
-  this.form.submit(function(e){
+
+  this.form.submit(function(e) {
     e.preventDefault();
     _this.sendBid($("#bid_value").val());
   });
-  this.socket.onmessage = function(e){
+
+  this.socket.onmessage = function(e) {
+    // bidok
+    // underbid
+    // outbid 123
+    // won
+    // lost
     var tokens = e.data.split(" ");
 
     switch(tokens[0]) {
@@ -39,9 +45,9 @@ AuctionSocket.prototype.initBinds = function(){
   };
 };
 
-AuctionSocket.prototype.sendBid = function(value){
+AuctionSocket.prototype.sendBid = function(value) {
   this.value = value;
-  var template = "bid {{user_id}} {{auction_id}} {{value}}";
+  var template = "bid {{auction_id}} {{user_id}} {{value}}";
   this.socket.send(Mustache.render(template, {
     user_id: this.user_id,
     auction_id: this.auction_id,
@@ -49,32 +55,30 @@ AuctionSocket.prototype.sendBid = function(value){
   }));
 };
 
-AuctionSocket.prototype.bid = function(){
+AuctionSocket.prototype.bid = function() {
   this.form.find(".message strong").html(
     "Your bid: " + this.value
   );
 };
 
-AuctionSocket.prototype.underbid = function(value){
+AuctionSocket.prototype.underbid = function(value) {
   this.form.find(".message strong").html(
-    "Your bid is under: " + value + "."
+    "Your bid is under " + value + "."
   );
 };
 
-AuctionSocket.prototype.outbid = function(value){
+AuctionSocket.prototype.outbid = function(value) {
   this.form.find(".message strong").html(
-    "You were outbid. It is now: " + value + "."
+    "You were outbid. It is now " + value + "."
   );
 };
 
-AuctionSocket.prototype.won = function(){
+AuctionSocket.prototype.won = function() {
   this.form.find(".message strong").html(
     "You won! " + this.value
   );
 };
 
-AuctionSocket.prototype.lost = function(){
-  this.form.find(".message strong").html(
-    "You lost the auction."
-  );
+AuctionSocket.prototype.lost = function() {
+  this.form.find(".message strong").html("You lost the auction.");
 };
